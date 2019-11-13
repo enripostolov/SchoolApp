5c40bbc2a084d3bf78716a89d592fcb605a749f7
Enrico Postolov <enrico.postolov@gmail.com>
2019-11-13 22:50:21 +0100

    Basic CRUD views for Parent and Student

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_183631) do
+ActiveRecord::Schema.define(version: 2019_11_13_212745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_11_13_183631) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "surname"
-    t.string "birth_date"
+    t.date "birth_date"
     t.string "fiscal_code"
     t.datetime "enrollment_date"
     t.datetime "created_at", precision: 6, null: false
